6648844595826acde199f3950b98ff2a6d0e9c69
paras <parasverma2531@gmail.com>
2023-02-02 22:41:42 +0530

    modules_filter_map_error_handling

---
+++ JAVASCRIPT/modules_maps_filter/utility.js
@@ -0,0 +1,5 @@
+import { addition } from "./utility_import_export";
+const a=5;
+const b=7;
+const ans=addition(a,b);
+console.log(ans);
---
+++ JAVASCRIPT/modules_maps_filter/utility_import_export.js
@@ -0,0 +1,4 @@
+export const addition=(a,b)=>
+{
+    return a+b;
+}

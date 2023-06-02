b86358572333cdf6663ecbd1e4b1a1c15fb306c3
paras <parasverma2531@gmail.com>
2023-06-02 21:52:26 +0530

    revisted dom

--- JAVASCRIPT/DOM/revisit.js
+++ JAVASCRIPT/DOM/revisit.js
@@ -1,2 +1,14 @@
-const box=document.getElementById("box");
-box.innerText="hey";
+const logMessage=(event)=>{
+    boxElement.style.backgroundColor='#080202';
+    boxElement.innerHTML=`${event.offsetX} ${event.offsetY}`
+}
+const container=document.getElementsByClassName("container");
+const boxElement=document.createElement('div');
+boxElement.classList.add('box');
+container[0].append(boxElement);
+// const addMessage=()=>{
+//     boxElement.innerText='HI';
+// }
+// boxElement.innerHTML=logMessage;
+// boxElement.addEventListener('click',logMessage);
+container[0].addEventListener('click',logMessage);

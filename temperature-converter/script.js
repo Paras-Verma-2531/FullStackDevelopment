874d208aa77e0d7223c9b2ced17cc22ec1ee0ce4
paras <parasverma2531@gmail.com>
2023-06-05 11:33:12 +0530

    script added

---
+++ temperature-converter/script.js
@@ -0,0 +1,7 @@
+// const getValue=()=>{
+//     const celcius=document.getElementById('Celcius').value;
+//    document.getElementById('Fehrenhiet').value=celcius;
+//    console.log(celcius);
+// }
+
+// document.getElementById('button').addEventListener('click',getValue);

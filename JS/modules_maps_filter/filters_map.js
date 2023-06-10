06438e3bad107ac0624b0ea823d68dd00a0d62a9
paras <parasverma2531@gmail.com>
2023-06-10 23:42:55 +0530

    Filters_map

--- JS/modules_maps_filter/filters_map.js
+++ JS/modules_maps_filter/filters_map.js
@@ -1,16 +1,20 @@
-const a=[1,2,3,4,5,6,7,8];
-function isEven(num)
-{
-    return num%2==0;
-}
-console.log(a.filter(isEven));
-function square(num)
-{
-    return num*num;
-}
-console.log(a.map(square));
-const StringArr=["paras","hello","verma","ab","bc"];
-console.log(StringArr.filter(element=>
-    {
-        return element.length<3;
-    }));
+// const a=[1,2,3,4,5,6,7,8];
+// function isEven(num)
+// {
+//     return num%2==0;
+// }
+// console.log(a.filter(isEven));
+// function square(num)
+// {
+//     return num*num;
+// }
+// console.log(a.map(square));
+// const StringArr=["paras","hello","verma","ab","bc"];
+// console.log(StringArr.filter(element=>
+//     {
+//         return element.length<3;
+//     }));
+const Names=["Paras","Akshita","Harsh","Aparna"];
+console.log(Names.filter((element)=>{
+    return element.length<6;
+}));

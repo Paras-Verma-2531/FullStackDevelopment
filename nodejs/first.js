e29548d4d9858568d3b4d266363e126d4929a969
paras verma <parasverma2531@gmail.com>
2023-07-11 18:30:01 +0530

    routes middleware MVC archi

--- nodejs/first.js
+++ nodejs/first.js
@@ -1,12 +1,19 @@
-// import the express
-const express=require('express');
-const app=express();
-//create an api end-point::
-app.get('/',(req,res)=>
-{
-    res.send("hello")
-})
-app.listen(4000,()=>
-{
-    console.log('listening on port 4000:');
-})
+// import the express with CJS --> common JS:
+const express = require("express");
+//configure the dotenv package :: used to access secret data which we dont want to make accessible to others directly
+require("dotenv").config({ path: "./.env" });
+const App = express();
+const postRouter = require("./routes/post");
+//create end points:
+App.get("/", (req, res) => {
+  res.send({
+    data: "ok",
+  });
+});
+//go to post route if endpoint is /post:
+App.use("/post", postRouter);
+const port = process.env.PORT;
+// Listen on the server:
+App.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
